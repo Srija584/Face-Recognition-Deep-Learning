083ec4fedae8a8a0d0128784303a7ec504717811
Srija584 <Srija584@users.noreply.github.com>
2019-01-19 19:07:15 +0530

    Update preProcess.m

--- preProcess.m
+++ preProcess.m
@@ -1,16 +1,16 @@
 D = '/home/srija/tensorflow/-45Images/auto_output/ori';
 clc;
-%  S = dir('*.jpg'); % pattern to match filenames.
-%  A = zeros( numel(S),128, 128 ); 
-%  for k = 1:numel(S)
-%    file2read = S(k).name;
-%    fid = imread(file2read);
-%    fid = imresize(fid, [128, 128]);
-%    fid = rgb2gray(fid);
-%    fid = im2double(fid);
-%    imshow(fid, [])
-%    frontal{k} = fid; % data from file
-%  end
+  S = dir('*.jpg'); % pattern to match filenames.
+  A = zeros( numel(S),128, 128 ); 
+  for k = 1:numel(S)
+    file2read = S(k).name;
+    fid = imread(file2read);
+    fid = imresize(fid, [128, 128]);
+    fid = rgb2gray(fid);
+    fid = im2double(fid);
+    imshow(fid, [])
+    frontal{k} = fid; % data from file
+  end
 % clear all;
 % clc;
 % for k =1:86
@@ -50,7 +50,62 @@ for k = 1:num_test
       % imshow(B, []);
     end
 end
+S = dir('*.jpg'); % pattern to match filenames.
+clc;
+ A = zeros( numel(S),128, 128 ); 
+ for k = 1:numel(S)
+   file2read = S(k).name;
+   fid = imread(file2read);
+   fid = imresize(fid, [128, 128]);
+   fid = rgb2gray(fid);
+   fid = im2double(fid);
+   imshow(fid, [])
+   frontal{k} = fid; % data from file
+ end
+ num_test = 144;
+for i=1:num_test
+    i
+    for j=1:12
+        P = generated{i,j};
+        Q = frontal{j};
+        c = corrcoef(P,Q);
+        c1 = P-Q;
+        f(i,j) = norm(sqrt(c1 * transpose(c1)));
+        results{i,j} = c(1,2);
+    end
+end
+f = f/max(max(f));
 
+% for i=1:86
+%     for j=1:12
+%         results{i,j} = results{i,j} + 1 - f(i,j);
+%     end
+% end
+results_cell = cell2mat(results);
+[~,finalMatch] = max(results_cell,[],2);
+m = 1;
+for i=1:12
+    for j=1:86
+        k = (i-1)*86 + j;
+        expected(k) = i;
+    end
+end
+clc
+count = 0;
+for i=1:num_test
+    if expected(i) == finalMatch(i)
+        count = count+1;
+    end
+end
+%counts{m} = (count/86)*100;
+(count/num_test)*100
+
+        
+        
+        
+        
+        
+        
 
 
 
