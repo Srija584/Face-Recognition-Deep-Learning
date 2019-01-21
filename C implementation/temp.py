93efbd7d70c593626b26a48ade9fb545c9100d26
Srija584 <Srija584@users.noreply.github.com>
2019-01-22 00:57:25 +0530

    Create temp.py
    Script to run code for 'n' num_persons

---
+++ C implementation/temp.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+import subprocess, sys
+
+num_persons = 9
+
+make_process = subprocess.Popen("make", stderr=subprocess.STDOUT)
+print "Compiling......"
+if make_process.wait() != 0:
+	print "Make failed. Exiting..."
+	sys.exit()
+print "Compilation complete."
+
+for itr in range(1,num_persons+1):
+	print "Executing for iteration : ", itr, " ......",
+        prediction_process = subprocess.Popen(["./cnn_weights", str(itr)], stderr=subprocess.STDOUT)
+	if prediction_process.wait() != 0:
+		print "\nError occurred while executing cnn_weights for iteration ", str(itr)
+		#sys.exit()
+        print "Success"
+ 	# print "Executing for iteration : ", itr, " ......",
+        # prediction_process = subprocess.Popen(["./cnn_prediction", str(itr)], stderr=subprocess.STDOUT)
+	# if prediction_process.wait() != 0:
+	# 	print "\nError occurred while executing cnn_prediction for iteration ", str(itr)
+	# 	sys.exit()
+        # print "Success"
+	
+for itr in range(1,num_persons+1):
+	print "Executing prediction iteration : ", itr, " ......",
+	for itr_sub in range(1, num_persons+1):
+		prediction_process = subprocess.Popen(["./cnn_prediction", str(itr), str(itr_sub)], stderr=subprocess.STDOUT)
+		if prediction_process.wait() != 0:
+			print "\nError occurred while executing cnn_weights for iteration ", str(itr)
+			#sys.exit()
+		print "Success"
